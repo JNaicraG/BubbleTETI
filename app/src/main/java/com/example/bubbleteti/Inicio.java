566ecefb8e35939c42680f977afe02ac452f269a
GarciaNJ <nicolas.jg.njg@gmail.com>
2022-05-15 21:48:04 -0300

    Main - Correção
    Implantação de CRUDE da empresa na main corretamente

--- app/src/main/java/com/example/bubbleteti/Inicio.java
+++ app/src/main/java/com/example/bubbleteti/Inicio.java
@@ -26,13 +26,11 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener {
 
     private Button bInicio;
     private FirebaseAuth mAuth;
-    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_inicio );
         mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
 
         bInicio = (Button) findViewById( R.id.btnInicio );
         bInicio.setOnClickListener( this );
@@ -42,49 +40,7 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        CriarEmpresa();
-        Toast.makeText( Inicio.this, "Dados da Empresa", Toast.LENGTH_LONG ).show();
-        //startActivity( new Intent(this, Login.class) );
+        startActivity( new Intent(this, Login.class) );
     }
-
-    private void CriarEmpresa(){
-        //Adicionar dados
-        Map<String,Object> empresa = new HashMap<>();
-        empresa.put("Nome Fantasia", "Empresa");
-        empresa.put("Nome","Empresa Ltda" );
-        empresa.put( "CNPJ", "XX.XXX.XXX/YYYY-AA");
-        String data = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-        empresa.put("Criação",data);
-
-        Toast.makeText( Inicio.this, "Dados da Empresa", Toast.LENGTH_LONG ).show();
-        //Colocar no BD
-        db.collection( "Empresas" ).add( empresa )
-                .addOnSuccessListener( new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d( "teste", "DocumentSnapshot added with ID: " + documentReference.getId());
-                        Toast.makeText( Inicio.this, "Sucesso ao criar a bagaça", Toast.LENGTH_LONG ).show();
-                    }
-                } )
-                .addOnFailureListener( new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("teste", "Error adding document", e);
-                    }
-                } );
-    }
-
-    private void AtualizarEmpresa(){
-        Map<String,Object> empresa = new HashMap<>();
-        empresa.put("Nome Fantasia", "Bubble.TETI");
-        empresa.put("Nome","BUBBLE TETI ASSOCIAÇÃO S.A" );
-        empresa.put( "CNPJ", "XX.XXX.XXX/YYYY-ZZ");
-        //String data = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-        //empresa.put("Criação",data);
-
-        db.collection( "Empresas" ).document("Empresa").update( empresa );
-
-    }
-
 
 }
--- app/src/main/java/com/example/bubbleteti/MainActivity.java
+++ app/src/main/java/com/example/bubbleteti/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bubbleteti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,15 +8,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button entrar;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        db = FirebaseFirestore.getInstance();
 
         entrar = (Button) findViewById( R.id.btnEntrar );
         entrar.setOnClickListener( this );
@@ -25,5 +41,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         Log.d( "Teste", "onClick: Entrando na tela de Login" );
         startActivity( new Intent(this, Login.class ) );
+        //CriarEmpresa();
+        AtualizarEmpresa();
+    }
+
+
+
+    private void CriarEmpresa(){
+        //Adicionar dados
+        Map<String,Object> empresa = new HashMap<>();
+        empresa.put("Nome Fantasia", "Empresa");
+        empresa.put("Nome","Empresa Ltda" );
+        empresa.put( "CNPJ", "XX.XXX.XXX/YYYY-AA");
+        String data = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        empresa.put("Criação",data);
+
+        //Colocar no BD
+        db.collection( "Empresas" ).document("Empresa").set( empresa );
+                /*.addOnSuccessListener( new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d( "teste", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText( MainActivity.this, "Sucesso ao criar a bagaça", Toast.LENGTH_LONG ).show();
+                    }
+                } )
+                .addOnFailureListener( new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("teste", "Error adding document", e);
+                    }
+                } );*/
+    }
+
+    private void AtualizarEmpresa(){
+        Map<String,Object> empresa = new HashMap<>();
+        empresa.put("Nome Fantasia", "Bubble.TETI");
+        empresa.put("Nome","BUBBLE TETI ASSOCIAÇÃO S.A" );
+        empresa.put( "CNPJ", "XX.XXX.XXX/YYYY-ZZ");
+        //String data = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        //empresa.put("Criação",data);
+
+        db.collection( "Empresas" ).document("Empresa").update( empresa );
+
     }
 }
